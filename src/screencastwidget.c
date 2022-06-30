GNOME/xdg-desktop-portal-gnome

commit f323c6b149381a09ef39c7ba343ac9550f3629c6

screencastwidget: Protect against uninstalled apps
Uninstalled apps (e.g. ones running from Builder) provide no desktop file,
which triggers a warning in the screencast widget code.

src/screencastwidget.c
@@ -487,7 +487,10 @@ screen_cast_widget_set_app_id (ScreenCastWidget *widget,
 
       id = g_strconcat (app_id, ".desktop", NULL);
       info = G_APP_INFO (g_desktop_app_info_new (id));
-      display_name = g_app_info_get_display_name (info);
+      if (info)
+        display_name = g_app_info_get_display_name (info);
+      else
+        display_name = g_strdup (app_id);
       monitor_heading = g_strdup_printf (_("Select monitor to share with %s"),
                                          display_name);
       window_heading = g_strdup_printf (_("Select window to share with %s"),

commit aad306cf55ec524e09802a4ecc334062ccd47208

screencastwidget: Don't leak the display_name string
Fallout from f323c6b149381a09ef39c7ba343ac9550f3629c6

https://gitlab.gnome.org/GNOME/xdg-desktop-portal-gnome/-/merge_requests/42

src/screencastwidget.c
@@ -538,7 +538,7 @@ screen_cast_widget_set_app_id (ScreenCastWidget *widget,
       if (info)
         display_name = g_app_info_get_display_name (info);
       else
-        display_name = g_strdup (app_id);
+        display_name = app_id;
       monitor_heading = g_strdup_printf (_("Select monitor to share with %s"),
                                          display_name);
       window_heading = g_strdup_printf (_("Select window to share with %s"),