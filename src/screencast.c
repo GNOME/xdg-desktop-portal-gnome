50c14bd646c57a2e1d4d542c67c6145f41d92b18
Peter Hutterer <peter.hutterer@who-t.net>
2023-08-29 13:38:49 +1000

    screencast: advertise the impl.portal ScreenCast version correctly

--- src/screencast.c
+++ src/screencast.c
@@ -834,6 +834,7 @@ on_gnome_screen_cast_enabled (GnomeScreenCast *gnome_screen_cast)
   g_autoptr(GError) error = NULL;
 
   impl = G_DBUS_INTERFACE_SKELETON (xdp_impl_screen_cast_skeleton_new ());
+  xdp_impl_screen_cast_set_version (XDP_IMPL_SCREEN_CAST (impl), 4);
 
   g_signal_connect (impl, "handle-create-session",
                     G_CALLBACK (handle_create_session), NULL);
