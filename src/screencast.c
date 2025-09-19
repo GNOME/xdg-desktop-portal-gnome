1d765f1a851c11a7065ebc698a92a743b48cc420
Marco Trevisan <mail@3v1n0.net>
2025-09-19 08:06:55 +0200

    screencast: Disconnect signal handlers on data free
    We may still receive dbus requests while the dialog handle has been
    free'd, so disconnect any signal bound to to it when free'ing it
    
    
    (cherry picked from commit 056029c6506dd1e7bd285943099e9fcc091cff8b)
    
    Co-authored-by: Marco Trevisan (Treviño) <mail@3v1n0.net>

--- src/screencast.c
+++ src/screencast.c
@@ -100,6 +100,8 @@ is_screen_cast_session (Session *session)
 static void
 screen_cast_dialog_handle_free (ScreenCastDialogHandle *dialog_handle)
 {
+  g_signal_handlers_disconnect_by_data (dialog_handle->request, dialog_handle);
+  g_signal_handlers_disconnect_by_data (dialog_handle->dialog, dialog_handle);
   g_clear_pointer (&dialog_handle->dialog, gtk_window_destroy);
   g_clear_object (&dialog_handle->external_parent);
   g_object_unref (dialog_handle->request);
